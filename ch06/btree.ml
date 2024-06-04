a7e562fd639f71bd8f4f584cd318dc8cbfa96359
hmsjwzb <hmsjwzb.zoho.com>
2024-06-05 07:55:49 +0800

    6.7 Ordered Sets

--- ch06/btree.ml
+++ ch06/btree.ml
@@ -93,6 +93,7 @@ let do_btree (f:'a -> unit) =
   in
   dof
 
+
 type comparison = Smaller | Equiv | Greater
 
 let mk_order ord x y =
@@ -264,7 +265,7 @@ exception Avl_exc of string
 exception Avl_search_exc of string
 
 let belongs_to_avl order =
-  belongs_to_bst (fun x y -> order (fst y))
+  belongs_to_bst (fun x y -> order x (fst y))
 
 let find_avl order e (t:('a * balance) btree) =
   try fst(find_bst (fun x y -> order x (fst y)) e t)
@@ -346,6 +347,8 @@ let rec add_to_avl option order t e =
 let add_list_to_avl option order = List.fold_left (add_to_avl option order)
 
 let mk_avl option order = add_list_to_avl option order Empty
+
+let do_avl f bt = do_btree (fun (t,_) -> f t) bt
 
 let merge_avl option order =
   it_btree (fun t x -> add_to_avl option order t (fst x))
@@ -453,3 +456,83 @@ let dict_merge opt d1 d2 =
   {dict_rel = d1.dict_rel;
    dict_data = merge_avl opt (fun x y -> d1.dict_rel (snd x) (snd y))
              d1.dict_data d2.dict_data}
+
+type 'a set =
+   {set_elements: 'a avltree;
+    set_order: 'a -> 'a -> comparison}
+
+let make_set c l =
+  {set_elements = mk_avl (fun x y -> x) c l;
+   set_order = c}
+
+let set_isempty s =
+  (s.set_elements = Empty)
+
+let set_member x s =
+  belongs_to_avl s.set_order x s.set_elements
+
+let set_it f s =
+  btree_it (fun x y -> f (fst x) y) s.set_elements
+
+let it_set f x s =
+  it_btree (fun x y -> f x (fst y)) x s.set_elements
+
+let do_set f s = do_avl f s.set_elements
+
+exception Set_exc of string
+
+let set_forall p s =
+  try
+    do_set (fun x -> if not (p x)
+                     then raise (Set_exc "")) s;
+    true
+  with  Set_exc _ -> false
+
+let set_exists p s =
+  try
+    do_set (fun x -> if (p x)
+                     then raise (Set_exc "")) s;
+    false
+  with Set_exc _ -> true
+
+let sub_set s1 s2 =
+  set_forall (fun e -> set_member e s2) s1
+
+let set_equiv s1 s2 =
+  (sub_set s1 s2) && (sub_set s2 s1)
+
+let list_of_set s =
+  flat_avl s.set_elements
+
+let set_random_element s =
+  fst (root s.set_elements)
+
+let add_to_set s x =
+  {set_elements = add_to_avl (fun x y -> x) s.set_order s.set_elements x;
+   set_order = s.set_order}
+
+let add_list_to_set s l =
+  List.fold_left add_to_set s l
+
+let remove_from_set s x =
+  try {set_elements = remove_from_avl s.set_order s.set_elements x;
+       set_order = s.set_order}
+  with _ -> raise (Set_exc "remove_from_set")
+
+let remove_list_from_set s = List.fold_left remove_from_set s
+
+let substract_from_set s x =
+  try remove_from_set s x
+  with _ -> s
+
+let set_union s1 s2 =
+  if not(s1.set_order = s2.set_order)
+  then raise (Set_exc "set_union: different set orders")
+  else it_set add_to_set s1 s2
+
+let set_diff s1 s2 =
+  if not(s1.set_order = s2.set_order)
+  then raise (Set_exc "set_diff: different set orders")
+  else it_set substract_from_set s1 s2
+
+let set_intersection s1 s2 = set_diff s1 (set_diff s1 s2)
